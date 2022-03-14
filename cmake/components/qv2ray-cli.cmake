5efdf263569126aa3ccfa03af61818e77b551b6a
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-03-14 20:27:20 +0800

    Update qv2ray-cli.cmake

--- cmake/components/qv2ray-cli.cmake
+++ cmake/components/qv2ray-cli.cmake
@@ -1,6 +1,6 @@
-set(QV2RAY_CLI_BASEDIR ${CMAKE_SOURCE_DIR}/src/ui/cli)
+set(QVMESSOCKET_CLI_BASEDIR ${CMAKE_SOURCE_DIR}/src/ui/cli)
 add_definitions(-DQAPPLICATION_CLASS=QCoreApplication)
 
-set(QV2RAY_CLI_SOURCES
-    ${QV2RAY_CLI_BASEDIR}/Qv2rayCliApplication.hpp
-    ${QV2RAY_CLI_BASEDIR}/Qv2rayCliApplication.cpp)
+set(QVMESSOCKET_CLI_SOURCES
+    ${QVMESSOCKET_CLI_BASEDIR}/Qv2rayCliApplication.hpp
+    ${QVMESSOCKET_CLI_BASEDIR}/Qv2rayCliApplication.cpp)
