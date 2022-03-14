03ed2e6cfb41a6f5aaa18dd288d74507b28833db
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-03-14 21:31:21 +0800

    Update qv2ray-ui-qml.cmake

--- cmake/components/qv2ray-ui-qml.cmake
+++ cmake/components/qv2ray-ui-qml.cmake
@@ -7,7 +7,7 @@ set(QVMESSOCKET_QML_SOURCES
     ${QVMESSOCKET_QML_BASEDIR}/Qv2rayQMLProperty.hpp
     )
 
-if(QV2RAY_QML_LIVE_UPDATE)
+if(QVMESSOCKET_QML_LIVE_UPDATE)
     add_definitions(-DQV2RAY_QMLLIVE_DEBUG=1)
     find_library(QMLLIVE_LIBS qmllive)
     link_libraries(-lqmllive)
