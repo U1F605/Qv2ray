3dcc9c102b7ae8a2a076e315b21a79cef59c7964
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-03-14 20:24:25 +0800

    Update qv2ray-base.cmake

--- cmake/components/qv2ray-base.cmake
+++ cmake/components/qv2ray-base.cmake
@@ -1,27 +1,27 @@
 set(QVMESSOCKET_BASEDIR_COMPONENTS ${CMAKE_SOURCE_DIR}/src/components)
-set(QV2RAY_BASEDIR_CORE ${CMAKE_SOURCE_DIR}/src/core)
-set(QV2RAY_BASEDIR_BASE ${CMAKE_SOURCE_DIR}/src/base)
+set(QVMESSOCKET_BASEDIR_CORE ${CMAKE_SOURCE_DIR}/src/core)
+set(QVMESSOCKET_BASEDIR_BASE ${CMAKE_SOURCE_DIR}/src/base)
 
 set(QV2RAY_BASE_SOURCES
-    ${QV2RAY_BASEDIR_BASE}/Qv2rayBaseApplication.cpp
-    ${QV2RAY_BASEDIR_BASE}/Qv2rayBaseApplication.hpp
+    ${QVMESSOCKET_BASEDIR_BASE}/Qv2rayBaseApplication.cpp
+    ${QVMESSOCKET_BASEDIR_BASE}/Qv2rayBaseApplication.hpp
     # Platform Dependent UI
     ${CMAKE_SOURCE_DIR}/src/ui/Qv2rayPlatformApplication.cpp
     ${CMAKE_SOURCE_DIR}/src/ui/Qv2rayPlatformApplication.hpp
     #
-    ${QV2RAY_BASEDIR_BASE}/JsonHelpers.hpp
-    ${QV2RAY_BASEDIR_BASE}/models/CoreObjectModels.hpp
-    ${QV2RAY_BASEDIR_BASE}/models/QvComplexConfigModels.hpp
-    ${QV2RAY_BASEDIR_BASE}/models/QvConfigIdentifier.hpp
-    ${QV2RAY_BASEDIR_BASE}/models/QvCoreSettings.hpp
-    ${QV2RAY_BASEDIR_BASE}/models/QvRuntimeConfig.hpp
-    ${QV2RAY_BASEDIR_BASE}/models/QvSafeType.hpp
-    ${QV2RAY_BASEDIR_BASE}/models/QvSettingsObject.hpp
-    ${QV2RAY_BASEDIR_BASE}/models/QvStartupConfig.hpp
+    ${QVMESSOCKET_BASEDIR_BASE}/JsonHelpers.hpp
+    ${QVMESSOCKET_BASEDIR_BASE}/models/CoreObjectModels.hpp
+    ${QVMESSOCKET_BASEDIR_BASE}/models/QvComplexConfigModels.hpp
+    ${QVMESSOCKET_BASEDIR_BASE}/models/QvConfigIdentifier.hpp
+    ${QVMESSOCKET_BASEDIR_BASE}/models/QvCoreSettings.hpp
+    ${QVMESSOCKET_BASEDIR_BASE}/models/QvRuntimeConfig.hpp
+    ${QVMESSOCKET_BASEDIR_BASE}/models/QvSafeType.hpp
+    ${QVMESSOCKET_BASEDIR_BASE}/models/QvSettingsObject.hpp
+    ${QVMESSOCKET_BASEDIR_BASE}/models/QvStartupConfig.hpp
     #
-    ${QV2RAY_BASEDIR_BASE}/Qv2rayBase.hpp
-    ${QV2RAY_BASEDIR_BASE}/Qv2rayFeatures.hpp
-    ${QV2RAY_BASEDIR_BASE}/Qv2rayLog.hpp
+    ${QVMESSOCKET_BASEDIR_BASE}/Qv2rayBase.hpp
+    ${QVMESSOCKET_BASEDIR_BASE}/Qv2rayFeatures.hpp
+    ${QVMESSOCKET_BASEDIR_BASE}/Qv2rayLog.hpp
     #
     ${CMAKE_SOURCE_DIR}/src/utils/HTTPRequestHelper.cpp
     ${CMAKE_SOURCE_DIR}/src/utils/HTTPRequestHelper.hpp
@@ -65,41 +65,41 @@ set(QV2RAY_BASE_SOURCES
     ${QVMESSOCKET_BASEDIR_COMPONENTS}/update/UpdateChecker.cpp
     ${QVMESSOCKET_BASEDIR_COMPONENTS}/update/UpdateChecker.hpp
     #
-    ${QV2RAY_BASEDIR_CORE}/connection/ConnectionIO.cpp
-    ${QV2RAY_BASEDIR_CORE}/connection/ConnectionIO.hpp
-    ${QV2RAY_BASEDIR_CORE}/connection/Generation.hpp
-    ${QV2RAY_BASEDIR_CORE}/connection/generation/final.cpp
-    ${QV2RAY_BASEDIR_CORE}/connection/generation/inbounds.cpp
-    ${QV2RAY_BASEDIR_CORE}/connection/generation/outbounds.cpp
-    ${QV2RAY_BASEDIR_CORE}/connection/generation/filters.cpp
-    ${QV2RAY_BASEDIR_CORE}/connection/generation/routing.cpp
-    ${QV2RAY_BASEDIR_CORE}/connection/generation/misc.cpp
-    ${QV2RAY_BASEDIR_CORE}/connection/Serialization.cpp
-    ${QV2RAY_BASEDIR_CORE}/connection/Serialization.hpp
-    ${QV2RAY_BASEDIR_CORE}/connection/serialization/ss.cpp
-    ${QV2RAY_BASEDIR_CORE}/connection/serialization/ssd.cpp
-    ${QV2RAY_BASEDIR_CORE}/connection/serialization/vmess.cpp
-    ${QV2RAY_BASEDIR_CORE}/connection/serialization/vmess_new.cpp
-    ${QV2RAY_BASEDIR_CORE}/connection/serialization/vless.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/connection/ConnectionIO.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/connection/ConnectionIO.hpp
+    ${QVMESSOCKET_BASEDIR_CORE}/connection/Generation.hpp
+    ${QVMESSOCKET_BASEDIR_CORE}/connection/generation/final.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/connection/generation/inbounds.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/connection/generation/outbounds.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/connection/generation/filters.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/connection/generation/routing.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/connection/generation/misc.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/connection/Serialization.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/connection/Serialization.hpp
+    ${QVMESSOCKET_BASEDIR_CORE}/connection/serialization/ss.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/connection/serialization/ssd.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/connection/serialization/vmess.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/connection/serialization/vmess_new.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/connection/serialization/vless.cpp
     #
-    ${QV2RAY_BASEDIR_CORE}/CoreUtils.cpp
-    ${QV2RAY_BASEDIR_CORE}/CoreUtils.hpp
+    ${QVMESSOCKET_BASEDIR_CORE}/CoreUtils.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/CoreUtils.hpp
     #
-    ${QV2RAY_BASEDIR_CORE}/handler/ConfigHandler.cpp
-    ${QV2RAY_BASEDIR_CORE}/handler/ConfigHandler.hpp
-    ${QV2RAY_BASEDIR_CORE}/handler/KernelInstanceHandler.cpp
-    ${QV2RAY_BASEDIR_CORE}/handler/KernelInstanceHandler.hpp
-    ${QV2RAY_BASEDIR_CORE}/handler/RouteHandler.cpp
-    ${QV2RAY_BASEDIR_CORE}/handler/RouteHandler.hpp
+    ${QVMESSOCKET_BASEDIR_CORE}/handler/ConfigHandler.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/handler/ConfigHandler.hpp
+    ${QVMESSOCKET_BASEDIR_CORE}/handler/KernelInstanceHandler.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/handler/KernelInstanceHandler.hpp
+    ${QVMESSOCKET_BASEDIR_CORE}/handler/RouteHandler.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/handler/RouteHandler.hpp
     #
-    ${QV2RAY_BASEDIR_CORE}/kernel/APIBackend.cpp
-    ${QV2RAY_BASEDIR_CORE}/kernel/APIBackend.hpp
-    ${QV2RAY_BASEDIR_CORE}/kernel/QvKernelABIChecker.cpp
-    ${QV2RAY_BASEDIR_CORE}/kernel/QvKernelABIChecker.hpp
-    ${QV2RAY_BASEDIR_CORE}/kernel/V2RayKernelInteractions.cpp
-    ${QV2RAY_BASEDIR_CORE}/kernel/V2RayKernelInteractions.hpp
+    ${QVMESSOCKET_BASEDIR_CORE}/kernel/APIBackend.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/kernel/APIBackend.hpp
+    ${QVMESSOCKET_BASEDIR_CORE}/kernel/QvKernelABIChecker.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/kernel/QvKernelABIChecker.hpp
+    ${QVMESSOCKET_BASEDIR_CORE}/kernel/V2RayKernelInteractions.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/kernel/V2RayKernelInteractions.hpp
     #
-    ${QV2RAY_BASEDIR_CORE}/settings/SettingsBackend.cpp
-    ${QV2RAY_BASEDIR_CORE}/settings/SettingsBackend.hpp
-    ${QV2RAY_BASEDIR_CORE}/settings/SettingsUpgrade.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/settings/SettingsBackend.cpp
+    ${QVMESSOCKET_BASEDIR_CORE}/settings/SettingsBackend.hpp
+    ${QVMESSOCKET_BASEDIR_CORE}/settings/SettingsUpgrade.cpp
     )
