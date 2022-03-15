0199817d719ac5135ccc05bd8bd3b16caa5d006a
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-03-15 18:20:01 +0800

    Update QRCodeHelper.hpp

--- src/ui/common/QRCodeHelper.hpp
+++ src/ui/common/QRCodeHelper.hpp
@@ -4,9 +4,9 @@ class QImage;
 class QSize;
 class QString;
 
-namespace Qv2ray::ui
+namespace Qvmessocket::ui
 {
     QString DecodeQRCode(const QImage &img);
     QImage EncodeQRCode(const QString content, int size);
 } // namespace Qv2ray::ui
-using namespace Qv2ray::ui;
+using namespace Qvmessocket::ui;
