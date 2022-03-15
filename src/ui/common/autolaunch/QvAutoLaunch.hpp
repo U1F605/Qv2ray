aacc0900447e596a0adafccebf7afb05ae50021c
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-03-15 18:16:56 +0800

    Update QvAutoLaunch.hpp

--- src/ui/common/autolaunch/QvAutoLaunch.hpp
+++ src/ui/common/autolaunch/QvAutoLaunch.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-namespace Qv2ray::components::autolaunch
+namespace Qvmessocket::components::autolaunch
 {
     bool GetLaunchAtLoginStatus();
     void SetLaunchAtLoginStatus(bool enable);
-} // namespace Qv2ray::components::autolaunch
+} // namespace Qvmessocket::components::autolaunch
 
-using namespace Qv2ray::components;
-using namespace Qv2ray::components::autolaunch;
+using namespace Qvmessocket::components;
+using namespace Qvmessocket::components::autolaunch;
