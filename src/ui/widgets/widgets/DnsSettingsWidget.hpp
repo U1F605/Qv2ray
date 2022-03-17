e2f31ec20ae0b6a8c050c073b5f44c4c15b37938
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-03-17 21:39:28 +0800

    Update DnsSettingsWidget.hpp

--- src/ui/widgets/widgets/DnsSettingsWidget.hpp
+++ src/ui/widgets/widgets/DnsSettingsWidget.hpp
@@ -4,7 +4,7 @@
 #include "ui/common/QvMessageBus.hpp"
 #include "ui_DnsSettingsWidget.h"
 
-namespace Qv2ray::ui::widgets
+namespace Qvmessocket::ui::widgets
 {
     class AutoCompleteTextEdit;
 }
@@ -53,8 +53,6 @@ class DnsSettingsWidget
     QvMessageBusSlotDecl;
     DNSObject dns;
     FakeDNSObject fakeDNS;
-    // int currentServerIndex;
-    //
-    Qv2ray::ui::widgets::AutoCompleteTextEdit *domainListTxt;
-    Qv2ray::ui::widgets::AutoCompleteTextEdit *ipListTxt;
+    Qvmessocket::ui::widgets::AutoCompleteTextEdit *domainListTxt;
+    Qvmessocket::ui::widgets::AutoCompleteTextEdit *ipListTxt;
 };
