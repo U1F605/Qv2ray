3aae2efd21f4da9686c5de778e43e286c77f1bfc
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-03-17 13:26:49 +0800

    Update QvGeositeReader.hpp

--- src/components/geosite/QvGeositeReader.hpp
+++ src/components/geosite/QvGeositeReader.hpp
@@ -1,10 +1,10 @@
 #pragma once
-#include "base/Qv2rayBase.hpp"
+#include "base/QvBase.hpp"
 
 namespace Qvmessocket::components::geosite
 {
     QStringList ReadGeoSiteFromFile(const QString &filepath);
-} // namespace Qv2ray::components::geosite
+}
 
 using namespace Qvmessocket::components;
 using namespace Qvmessocket::components::geosite;
