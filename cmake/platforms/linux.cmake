c5f64272723101bce8cd92003cd866eb196ba2a4
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-03-22 17:16:25 +0800

    Update linux.cmake

--- cmake/platforms/linux.cmake
+++ cmake/platforms/linux.cmake
@@ -5,6 +5,8 @@ install(FILES ${CMAKE_SOURCE_DIR}/assets/icons/qv2ray.svg DESTINATION share/icon
 
 set(QVMESSOCKET_ICON_DIMENSIONS 16 22 32 48 64 128 256 512 1024)
 
+set(APPS "\${CMAKE_INSTALL_PREFIX}/qvmessocket")
+
 foreach(d ${QVMESSOCKET_ICON_DIMENSIONS})
     install(FILES assets/icons/qv2ray.${d}.png
         DESTINATION share/icons/hicolor/${d}x${d}/apps
