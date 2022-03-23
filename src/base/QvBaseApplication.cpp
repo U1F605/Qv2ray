218af6e8db404104ad50461328f57a008c916425
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-03-23 23:23:46 +0800

    Update QvBaseApplication.cpp

--- src/base/QvBaseApplication.cpp
+++ src/base/QvBaseApplication.cpp
@@ -14,11 +14,11 @@ QvApplicationInterface::QvApplicationInterface()
 {
     ConfigObject = new QvConfigObject;
     QvCoreApplication = this;
-    LOG("Qv2ray", QVMESSOCKET_VERSION_STRING, "on", QSysInfo::prettyProductName(), QSysInfo::currentCpuArchitecture());
-    DEBUG("Qv2ray Start Time: ", QTime::currentTime().msecsSinceStartOfDay());
+    LOG(QVMESSOCKET_VERSION_STRING, "on", QSysInfo::prettyProductName(), QSysInfo::currentCpuArchitecture());
+    DEBUG("Start Time: ", QTime::currentTime().msecsSinceStartOfDay());
     DEBUG("QVMESSOCKET_BUILD_INFO", QVMESSOCKET_BUILD_INFO);
     DEBUG("QVMESSOCKET_BUILD_EXTRA_INFO", QVMESSOCKET_BUILD_EXTRA_INFO);
-    DEBUG("QV2RAY_BUILD_NUMBER", QSTRN(QVMESSOCKET_VERSION_BUILD));
+    DEBUG("QVMESSOCKET_BUILD_NUMBER", QSTRN(QVMESSOCKET_VERSION_BUILD));
     QStringList licenseList;
     licenseList << "This program comes with ABSOLUTELY NO WARRANTY.";
     licenseList << "This is free software, and you are welcome to redistribute it";
