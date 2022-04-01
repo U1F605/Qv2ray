bc1edeef4cb8eb527c9585fb474b0af82f3ca366
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-04-01 19:48:38 +0800

    Update DnsSettingsWidget.hpp

--- src/ui/widgets/widgets/DnsSettingsWidget.hpp
+++ src/ui/widgets/widgets/DnsSettingsWidget.hpp
@@ -18,7 +18,7 @@ class DnsSettingsWidget
   public:
     explicit DnsSettingsWidget(QWidget *parent = nullptr);
     void SetDNSObject(const DNSObject &dns);
-    std::pair<DNSObject, FakeDNSObject> GetDNSObject();
+    std::pair<DNSObject> GetDNSObject();
     bool CheckIsValidDNS() const;
 
   private slots:
