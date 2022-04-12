90b777056765bce0d866b986b880bad5fbf016cb
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-04-12 18:21:02 +0800

    Update QvPlugin-BuiltinProtocolSupport.cmake

--- src/plugins/protocols/QvPlugin-BuiltinProtocolSupport.cmake
+++ src/plugins/protocols/QvPlugin-BuiltinProtocolSupport.cmake
@@ -17,16 +17,12 @@ ADD_SOURCE(outbound freedom)
 ADD_SOURCE(outbound httpout)
 ADD_SOURCE(outbound socksout)
 ADD_SOURCE(outbound shadowsocks)
-ADD_SOURCE(outbound vless)
 ADD_SOURCE(outbound vmess)
 ADD_SOURCE(outbound loopback)
 
 ADD_SOURCE(inbound dokodemo-door)
 ADD_SOURCE(inbound httpin)
 ADD_SOURCE(inbound socksin)
-#ADD_SOURCE(inbound shadowsocks)
-#ADD_SOURCE(inbound vless)
-#ADD_SOURCE(inbound vmess)
 
 set(BUILTIN_PROTOCOL_PLUGIN_SOURCES
     ${PLUGIN_UI_SOURCE}
@@ -54,23 +50,10 @@ target_include_directories(${PROTOCOL_PLUGIN_TARGET} PRIVATE ${QVPLUGIN_INTERFAC
 target_include_directories(${PROTOCOL_PLUGIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR})
 target_include_directories(${PROTOCOL_PLUGIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/../common)
 
-if(UNIX AND NOT APPLE AND NOT WIN32 AND NOT ANDROID)
+if(UNIX)
     install(TARGETS ${PROTOCOL_PLUGIN_TARGET} LIBRARY DESTINATION share/qv2ray/plugins)
 elseif(WIN32)
     install(TARGETS ${PROTOCOL_PLUGIN_TARGET} LIBRARY DESTINATION plugins)
-elseif(APPLE)
-    add_custom_command(TARGET ${PROTOCOL_PLUGIN_TARGET}
-        POST_BUILD
-        COMMAND ${CMAKE_INSTALL_NAME_TOOL} -add_rpath "@executable_path/../Frameworks/" $<TARGET_FILE:${PROTOCOL_PLUGIN_TARGET}>)
-    install(TARGETS ${PROTOCOL_PLUGIN_TARGET} LIBRARY DESTINATION ${CMAKE_INSTALL_PREFIX}/qv2ray.app/Contents/Resources/plugins)
-elseif(ANDROID)
-    set(deployment_tool "${QT_HOST_PATH}/${QT6_HOST_INFO_BINDIR}/androiddeployqt")
-    set(apk_dir "$<TARGET_PROPERTY:${PROTOCOL_PLUGIN_TARGET},BINARY_DIR>/android-build")
-    add_custom_command(TARGET ${PROTOCOL_PLUGIN_TARGET} POST_BUILD
-        COMMAND
-        ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${PROTOCOL_PLUGIN_TARGET}>
-        "${apk_dir}/libs/${CMAKE_ANDROID_ARCH_ABI}/$<TARGET_FILE_NAME:${PROTOCOL_PLUGIN_TARGET}>"
-        )
 else()
     message(FATAL_ERROR "?")
 endif()
