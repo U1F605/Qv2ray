c6c28b629ab331db565760911629911008265732
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-04-12 18:31:31 +0800

    Update BuiltinProtocolPlugin.hpp

--- src/plugins/protocols/BuiltinProtocolPlugin.hpp
+++ src/plugins/protocols/BuiltinProtocolPlugin.hpp
@@ -22,8 +22,8 @@ class InternalProtocolSupportPlugin
         return { "Builtin Protocol Support",                                                  //
                  "Qv2ray Core Workgroup",                                                     //
                  "qvplugin_builtin_protocol",                                                 //
-                 "VMess, VLESS, SOCKS, HTTP, Shadowsocks, DNS, Dokodemo-door editor support", //
-                 QVMESSOCKET_VERSION_STRING,                                                       //
+                 "VMess, SOCKS, HTTP, Shadowsocks, DNS, Dokodemo-door editor support",        //
+                 QVMESSOCKET_VERSION_STRING,                                                  //
                  "Qv2ray/Qv2ray",                                                             //
                  {
                      COMPONENT_OUTBOUND_HANDLER, //
