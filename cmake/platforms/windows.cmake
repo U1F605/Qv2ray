89c66f3a325405fcafc26bf6cc79b6789b0791fa
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-04-16 19:36:01 +0800

    Update windows.cmake

--- cmake/platforms/windows.cmake
+++ cmake/platforms/windows.cmake
@@ -15,7 +15,7 @@ if(QVMESSOCKET_AUTO_DEPLOY)
     else()
         set(QVMESSOCKET_QtX_DIR ${Qt5_DIR})
     endif()
-    add_custom_command(TARGET qvmessocket
+    add_custom_command(TARGET Qvmessocket
         POST_BUILD
         COMMAND ${QVMESSOCKET_QtX_DIR}/../../../bin/windeployqt ${CMAKE_BINARY_DIR}/qvmessocket.exe --compiler-runtime --verbose 2 --dir ${CMAKE_BINARY_DIR}/winqt/)
 endif()
