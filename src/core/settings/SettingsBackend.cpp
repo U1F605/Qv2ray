e47f83733e6b59777963ca694e58052266a61846
emp3826 <30045531+emp3826@users.noreply.github.com>
2022-06-12 01:06:20 +0800

    Update SettingsBackend.cpp

--- src/core/settings/SettingsBackend.cpp
+++ src/core/settings/SettingsBackend.cpp
@@ -212,7 +212,7 @@ namespace Qvmessocket::core::config
                 return false;
             }
 
-            GlobalConfig.kernelConfig.KernelPath(QVMESSOCKET_DEFAULT_VCORE_PATH);
+            GlobalConfig.kernelConfig.KernelPath(QCLASH_DEFAULT_VCORE_PATH);
             GlobalConfig.kernelConfig.AssetsPath(QCLASH_DEFAULT_VASSETS_PATH);
             GlobalConfig.logLevel = 3;
             GlobalConfig.uiConfig.language = QLocale::system().name();
