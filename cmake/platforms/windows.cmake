ce1c2c4128bd37460f52ce4a9ad694621a25b6cb
emp3826 <30045531+emp3826@users.noreply.github.com>
2022-06-12 12:18:58 +0800

    Update windows.cmake

--- cmake/platforms/windows.cmake
+++ cmake/platforms/windows.cmake
@@ -10,11 +10,7 @@ set(APPS "\${CMAKE_INSTALL_PREFIX}/Qvmessocket.exe")
 include(cmake/deployment.cmake)
 
 if(QCLASH_AUTO_DEPLOY)
-    if(QVMESSOCKET_QT6)
-        set(QVMESSOCKET_QtX_DIR ${Qt6_DIR})
-    else()
-        set(QVMESSOCKET_QtX_DIR ${Qt5_DIR})
-    endif()
+    set(QVMESSOCKET_QtX_DIR ${Qt6_DIR})
     add_custom_command(TARGET Qvmessocket
         POST_BUILD
         COMMAND ${QVMESSOCKET_QtX_DIR}/../../../bin/windeployqt ${CMAKE_BINARY_DIR}/qvmessocket.exe --compiler-runtime --verbose 2 --dir ${CMAKE_BINARY_DIR}/winqt/)
