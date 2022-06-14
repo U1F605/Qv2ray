661b1240dcf628377077da3089a76a9b9879fd54
emp3826 <30045531+emp3826@users.noreply.github.com>
2022-06-14 22:46:40 +0800

    Update windows.cmake

--- cmake/platforms/windows.cmake
+++ cmake/platforms/windows.cmake
@@ -1,7 +1,7 @@
 find_package(OpenSSL REQUIRED)
 target_link_libraries(Qclash_baselib wininet wsock32 ws2_32 user32 Rasapi32 Iphlpapi OpenSSL::SSL OpenSSL::Crypto Dbghelp)
 
-install(TARGETS Qvmessocket RUNTIME DESTINATION .)
+install(TARGETS Qclash RUNTIME DESTINATION .)
 
 install(DIRECTORY ${CMAKE_BINARY_DIR}/winqt/ DESTINATION .)
 
@@ -11,7 +11,7 @@ include(cmake/deployment.cmake)
 
 if(QCLASH_AUTO_DEPLOY)
     set(QVMESSOCKET_QtX_DIR ${Qt6_DIR})
-    add_custom_command(TARGET Qvmessocket
+    add_custom_command(TARGET Qclash
         POST_BUILD
         COMMAND ${QVMESSOCKET_QtX_DIR}/../../../bin/windeployqt ${CMAKE_BINARY_DIR}/qvmessocket.exe --compiler-runtime --verbose 2 --dir ${CMAKE_BINARY_DIR}/winqt/)
 endif()
